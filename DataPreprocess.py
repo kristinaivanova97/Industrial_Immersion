9c749df589ffbed035d0511b450ca2639c63c3c1
kristinaivanova97 <krapinka_krapinka@mail.ru>
2020-07-17 12:35:30 +0300

    Files for training

---
+++ DataPreprocess.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+import re
+from transformers import BertTokenizer
+from tqdm import tqdm
+import math
+
+path_to_train = './data/tsya_data/train_data_bklif.csv'
+path_to_val = './data/tsya_data/val_data_bklif.csv'
+path_to_train_labels = 'Labels.txt'
+path_to_val_labels = 'Val_labels.txt'
+
+class DataPreprocess:
+    
+    def __init__(self, path_to_file):
+        
+        label_list = ["[Padding]", "[SEP]", "[CLS]", "O","ться", "тся"]
+        self.label_map = {}
+        for (i, label) in enumerate(label_list):
+            self.label_map[label] = i
+            
+        self.input_ids = []
+        self.attention_masks = []
+        self.label_ids = []
+        self.nopad = []
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=True)
+        self.data = pd.read_csv(path_to_file, index_col=0)
+        self.data.reset_index(drop=True, inplace=True)
+        y, self.data = self.__label_dict()  # [num of sentense, num of word], contain labels
+        
+        self.y_label = []
+        for i in y.keys():
+            raw = []
+            for j in range(len(y[i])):
+                raw.append(y[i][j])
+            self.y_label.append(raw)
+            
+    # Create labels for every word (or comma) in text
+    def __label_dict(self):
+        tsya_search = re.compile(r'тся\b')
+        tsiya_search = re.compile(r'ться\b')
+        dicty = {}
+        k = 0
+        i = 0
+        new_data_x = []
+        for raw, label in zip(self.data['x'], self.data['y']):
+            m = tsya_search.findall(raw)
+            m2 = tsiya_search.findall(raw)
+            if label == 1 or len(m) + len(m2) == 1:
+                new_data_x.append(raw)
+                #for j,word in enumerate(raw.split()):
+                for j, word in  enumerate(re.findall(r'\w+|[^\w\s]', raw, re.UNICODE)):
+                    # any sequence of letters(+numbers) or comas, points
+                    m = tsya_search.findall(word)
+                    m2 = tsiya_search.findall(word)
+                    dicty.setdefault(i, {})
+                    if (len(m) == 1 and label == 1) or (len(m2) == 1 and label == 0):
+                        dicty[i][j] = "тся"
+                    elif (len(m) == 1 and label == 0) or (len(m2) == 1 and label == 1):
+                        dicty[i][j] = "ться"
+                    else:
+                        dicty[i][j] = "O"
+            else:
+                k+=1
+            i+=1
+        print("Num of sentences which have a mistake and contain more than 1 word with ться/тся = ", k)
+        return dicty, new_data_x
+    
+    def __process(self):
+        
+        for k,raw in tqdm(enumerate(self.data)):
+            input_ids, input_mask,label_ids, nopad = self.__convert_single_example(raw, self.y_label[k])
+            self.input_ids.append(input_ids)
+            self.attention_masks.append(input_mask)
+            self.label_ids.append(label_ids)
+            self.nopad.append(nopad)
+
+    # find indices for Bert from our data for each sentence
+    def __convert_single_example(self, text, y_label, max_seq_length = 512):
+
+        #textlist = text.split()
+        textlist = re.findall(r'\w+|[^\w\s]', text, re.UNICODE)
+        tokens = []
+        labels = []
+        nopad = []
+        for i, word in enumerate(textlist):
+            token = self.tokenizer.tokenize(word)
+            tokens.extend(token)
+            label_1 = y_label[i]
+            for m in range(len(token)):
+                labels.append(label_1)
+        if len(tokens) >= max_seq_length - 1:
+            tokens = tokens[0:(max_seq_length - 2)]
+            labels = labels[0:(max_seq_length - 2)]
+        ntokens = []
+        label_ids = []
+        ntokens.append("[CLS]")
+        label_ids.append(self.label_map["[CLS]"])
+        for i, token in enumerate(tokens):
+            ntokens.append(token)
+            label_ids.append(self.label_map[labels[i]])
+
+        ntokens.append("[SEP]")
+        nopad.append(len(ntokens))
+        label_ids.append(self.label_map["[SEP]"])
+        input_ids = self.tokenizer.convert_tokens_to_ids(ntokens)
+        input_mask = [1] * len(input_ids)
+
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            label_ids.append(0)
+            ntokens.append("[Padding]")
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(label_ids) == max_seq_length
+        return input_ids, input_mask, label_ids, nopad
+    
+    def saveLabels(self, path):
+ 
+        MyFile = open(path, 'w')
+        for raw in tqdm(self.y_label):
+            for elem in raw:
+                MyFile.write(str(elem))
+                MyFile.write(' ')
+            MyFile.write('\n')
+        MyFile.close()
+        
+    def saveIndices(self, ftype):
+        self.__process()
+        # save to 3 files
+        file_names = ['input_ids_' + ftype + '.txt', 'input_mask_' + ftype + '.txt', 'label_ids_' + ftype + '.txt']
+        features = [self.input_ids, self.attention_masks, self.label_ids]
+
+        for j in range(len(file_names)):
+            MyFile = open (file_names[j], 'w')
+            for raw in features[j]:
+                for elem in raw:
+                    MyFile.write(str(elem))
+                    MyFile.write(' ')
+                MyFile.write('\n')
+            MyFile.close()
+            
+TrainProcessor = DataPreprocess(path_to_file = path_to_train)
+ValProcessor = DataPreprocess(path_to_file = path_to_val)
+TrainProcessor.saveLabels(path = path_to_train_labels)
+ValProcessor.saveLabels(path = path_to_val_labels)
+TrainProcessor.saveIndices(ftype = 'train')
+ValProcessor.saveIndices(ftype = 'val')
+
+"""
+# To check the max length of sentences in text
+# For every sentence...
+max_len = 0
+new = TrainProcessor.data['x'].str.strip()
+for sent in new:
+    # Tokenize the text and add `[CLS]` and `[SEP]` tokens.
+    input_ids = tokenizer.encode(sent, add_special_tokens=True)
+    max_len = max(max_len, len(input_ids))
+
+print('Max sentence length: ', max_len)
+"""
---
+++ Train.py
@@ -0,0 +1,305 @@
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+import numpy as np
+import pandas as pd
+import torch
+from transformers import BertTokenizer, BertForTokenClassification, BertConfig
+from transformers import AdamW, get_linear_schedule_with_warmup
+from torch.utils.data import TensorDataset, random_split, DataLoader, RandomSampler, SequentialSampler
+import time
+import datetime
+import random
+
+batch_size = 10
+epochs = 3 # The BERT authors recommend between 2 and 4.
+max_seq_length = 512 # for bert this limit exists
+label_list = ["[Padding]", "[SEP]", "[CLS]", "O","ться", "тся"] # all possible labels
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Device: {device}")
+
+# input indices are created in another file (DataPreprocess.py) - essential for Bert model
+class GetIndices:
+    
+    def __init__(self, ftype):
+        
+        self.file_names = ['input_ids_' + ftype + '.txt', 'input_mask_' + ftype + '.txt', 'label_ids_' + ftype + '.txt']
+        self.input_ids = []
+        self.input_mask = []
+        self.label_ids = []
+        
+    def Upload(self):
+        
+        features = [self.input_ids, self.input_mask, self.label_ids]
+        for i in range(len(self.file_names)):
+            my_file = open(self.file_names[i], 'r')
+            list_of_lists = []
+            for line in my_file:
+                stripped_line = line.strip()
+                line_list = stripped_line.split()
+                list_of_lists.append(line_list)
+
+            my_file.close()
+            for j in range(len(list_of_lists)):
+                features[i].append(list(map(int, list_of_lists[j])))
+
+    
+    def GetLabels(self, filename):
+        
+        # *** not nessesary as wont't be used later ***
+        my_file = open(filename, 'r') # 'Labels.txt'
+        y_label = []
+        for line in my_file:
+            stripped_line = line.strip()
+            line_list = stripped_line.split()
+            y_label.append(line_list)
+        my_file.close()
+        print("Size of y_label = ", len(y_label))
+        print("*** pleminary labels are created ***")
+        
+        return y_label
+
+TrainProcessor = GetIndices(ftype = 'train')
+ValProcessor = GetIndices(ftype = 'val')
+TrainProcessor.Upload()
+ValProcessor.Upload()
+
+assert len(TrainProcessor.input_ids[0]) == max_seq_length
+assert len(TrainProcessor.input_mask[0]) == max_seq_length
+assert len(TrainProcessor.label_ids[0]) == max_seq_length
+
+print("Sequense len = ", len(TrainProcessor.input_ids[0]))
+print("Num of sequences = ", len(TrainProcessor.input_ids))
+print("Num of val sequences = ", len(ValProcessor.input_ids))
+print("files with input ids, masks, segment ids and label ids are loaded succesfully")
+
+class TsyaModelTrain:
+    
+    def __init__(self, epochs = epochs):
+    
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=True)
+        self.model = BertForTokenClassification.from_pretrained(
+            'bert-base-multilingual-cased', # Use the 12-layer BERT model, with an uncased vocab.
+            num_labels = len(label_list), # The number of output labels
+            output_attentions = False, # Whether the model returns attentions weights.
+            output_hidden_states = False, # Whether the model returns all hidden-states.
+        )
+        self.model.to(device)
+
+        self.optimizer = AdamW(self.model.parameters(),
+                          lr = 2e-5, # args.learning_rate - default is 5e-5
+                          eps = 1e-8 # args.adam_epsilon  - default is 1e-8.
+                         )
+        
+        self.train_dataloader, self.validation_dataloader = self.__Dataset()
+        # Total number of training steps is [number of batches] x [number of epochs].
+        # (Note that this is not the same as the number of training samples).
+        total_steps = len(self.train_dataloader) * epochs
+
+        # Create the learning rate scheduler.
+        self.scheduler = get_linear_schedule_with_warmup(self.optimizer, num_warmup_steps = 0, num_training_steps = total_steps)
+        
+        self.params = list(self.model.named_parameters())
+
+        print('==== Embedding Layer ====\n')
+
+        for p in self.params[0:5]:
+            print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+
+        print('\n==== Output Layer ====\n')
+
+        for p in self.params[-4:]:
+            print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+        
+    def __Dataset(self, TrainProcessor = TrainProcessor, ValProcessor = ValProcessor):
+            
+        # Combine the training inputs into a TensorDataset.
+
+        dataset = TensorDataset(torch.tensor(TrainProcessor.input_ids)[:10000],
+                                torch.tensor(TrainProcessor.input_mask)[:10000],
+                                torch.tensor(TrainProcessor.label_ids)[:10000])
+
+        val_dataset = TensorDataset(torch.tensor(ValProcessor.input_ids[:10000]),
+                                    torch.tensor(ValProcessor.input_mask[:10000]),
+                                    torch.tensor(ValProcessor.label_ids[:10000]))
+
+        train_dataloader = DataLoader(dataset, sampler = RandomSampler(dataset), batch_size = batch_size)
+        validation_dataloader = DataLoader(val_dataset, sampler = SequentialSampler(val_dataset), batch_size = batch_size)
+
+        return train_dataloader, validation_dataloader
+    def format_time(self, elapsed):
+        
+        '''
+        Takes a time in seconds and returns a string hh:mm:ss
+        '''
+        elapsed_rounded = int(round((elapsed)))
+        return str(datetime.timedelta(seconds=elapsed_rounded))
+
+    # Function to calculate the accuracy of our predictions vs labels
+    def flat_accuracy(self, preds, labels):
+        
+        pred_flat = np.argmax(preds, axis=2).flatten()
+        labels_flat = labels.flatten()
+        return np.sum(pred_flat[labels_flat!=0] == labels_flat[labels_flat!=0]) / len(labels_flat[labels_flat!=0])
+    
+    def train(self, epochs = epochs):
+        
+        # This training code is based on the `run_glue.py` script here:
+        # https://github.com/huggingface/transformers/blob/5bfcd0485ece086ebcbed2d008813037968a9e58/examples/run_glue.py#L128
+        seed_val = 42
+        random.seed(seed_val)
+        np.random.seed(seed_val)
+        torch.manual_seed(seed_val)
+        torch.cuda.manual_seed_all(seed_val)
+
+        training_stats = []
+        total_t0 = time.time()
+
+        for epoch_i in range(epochs):
+
+            # ========================================
+            #               Training
+            # ========================================
+
+            print("")
+            print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+            print('Training...')
+
+            t0 = time.time()
+            total_train_loss = 0
+
+            # vs. test (source: https://stackoverflow.com/questions/51433378/what-does-model-train-do-in-pytorch)
+            self.model.train()
+
+            for step, batch in enumerate(self.train_dataloader):
+                # Progress update every 40 batches.
+                if step % 40 == 0 and not step == 0:
+                    # Calculate elapsed time in minutes.
+                    elapsed = self.format_time(time.time() - t0)
+                    
+                    # Report progress.
+                    print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(self.train_dataloader), elapsed))
+
+                # Unpack this training batch from our dataloader.
+                # `batch` contains three pytorch tensors:
+                #   [0]: input ids
+                #   [1]: attention masks
+                #   [2]: labels
+                b_input_ids = batch[0].to(device)
+                b_input_mask = batch[1].to(device)
+                b_labels = batch[2].to(device)
+                # (source: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch)
+                self.model.zero_grad()
+
+                # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
+                # It returns different numbers of parameters depending on what arguments
+                # arge given and what flags are set. For our useage here, it returns
+                # the loss (because we provided labels) and the "logits"--the model
+                # outputs prior to activation.
+                loss, logits = self.model(b_input_ids,
+                                     token_type_ids=None, #b_segment,
+                                     attention_mask=b_input_mask,
+                                     labels=b_labels)
+
+                total_train_loss += loss.item()
+                loss.backward()
+
+                # Clip the norm of the gradients to 1.0.
+                # This is to help prevent the "exploding gradients" problem.
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+
+                self.optimizer.step()
+                self.scheduler.step()
+            avg_train_loss = total_train_loss / len(self.train_dataloader)
+
+            training_time = self.format_time(time.time() - t0)
+
+            # ========================================
+            #               Validation
+            # ========================================
+            # After the completion of each training epoch, measure our performance on
+            # our validation set.
+
+            print("")
+            print("Running Validation...")
+
+            t0 = time.time()
+
+            # Put the model in evaluation mode--the dropout layers behave differently
+            # during evaluation.
+            self.model.eval()
+
+            total_eval_accuracy = 0
+            total_eval_loss = 0
+            nb_eval_steps = 0
+
+            # Evaluate data for one epoch
+            for batch in self.validation_dataloader:
+
+                b_input_ids = batch[0].to(device)
+                b_input_mask = batch[1].to(device)
+                b_labels = batch[2].to(device)
+
+                # Tell pytorch not to bother with constructing the compute graph during
+                # the forward pass, since this is only needed for backprop (training).
+                with torch.no_grad():
+
+                    # Forward pass, calculate logit predictions.
+                    # token_type_ids is the same as the "segment ids", which
+                    # differentiates sentence 1 and 2 in 2-sentence tasks.
+                    # The documentation for this `model` function is here:
+                    # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
+                    # Get the "logits" output by the model. The "logits" are the output
+                    # values prior to applying an activation function like the softmax.
+                    (loss, logits) = self.model(b_input_ids,
+                                           token_type_ids=None, #b_segment,
+                                           attention_mask=b_input_mask,
+                                           labels=b_labels)
+
+                total_eval_loss += loss.item()
+                logits = logits.detach().cpu().numpy()
+                label_ids = b_labels.to('cpu').numpy()
+
+                # Calculate the accuracy for this batch of test sentences, and
+                # accumulate it over all batches.
+                total_eval_accuracy += self.flat_accuracy(logits, label_ids)
+            print(self.tokenizer.convert_ids_to_tokens(b_input_ids[0, :]))
+            last = np.argmax(logits, axis=2)
+            part = last[0,:]
+            part_true = label_ids[0,:]
+            print("Last true = ", part_true)
+            print("Last prediction", part)
+
+            avg_val_accuracy = total_eval_accuracy / len(self.validation_dataloader)
+            print("  Accuracy: {0:.3f}".format(avg_val_accuracy))
+
+            # Calculate the average loss over all of the batches.
+            avg_val_loss = total_eval_loss / len(self.validation_dataloader)
+
+            # Measure how long the validation run took.
+            validation_time = self.format_time(time.time() - t0)
+
+            print("  Validation Loss: {0:.3f}".format(avg_val_loss))
+            print("  Validation took: {:}".format(validation_time))
+
+            # Record all statistics from this epoch.
+            training_stats.append(
+                {
+                    'epoch': epoch_i + 1,
+                    'Training Loss': avg_train_loss,
+                    'Valid. Loss': avg_val_loss,
+                    'Valid. Accur.': avg_val_accuracy,
+                    'Training Time': training_time,
+                    'Validation Time': validation_time
+                }
+            )
+
+            print("  Average training loss: {0:.3f}".format(avg_train_loss))
+            print("  Training epoch took: {:}".format(training_time))
+            torch.save(self.model.state_dict(), "Chkpt.pth")
+        print("Training complete!")
+        print("Total training took {:} (h:mm:ss)".format(self.format_time(time.time()-total_t0)))
+        torch.save(self.model.state_dict(), "Chkpt.pth")
+
+model = TsyaModelTrain()
+model.train()
