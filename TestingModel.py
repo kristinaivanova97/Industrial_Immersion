0d97286799b2e4e876c4f468868f732b78a96646
kristinaivanova97 <krapinka_krapinka@mail.ru>
2020-07-28 13:58:32 +0300

    Checking for tsya/nn occurence before predict

--- TestingModel.py
+++ TestingModel.py
@@ -12,6 +12,26 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Device: {device}")
 weight_path = "Chkpt2.pth"
     
+def check_contain_tsya_or_nn(data):
+    
+    data_with_tsya_or_nn = []
+    tsya_search = re.compile(r'тся\b')
+    tsiya_search = re.compile(r'ться\b')
+    n_search = re.compile(r'\wн*\w\b')
+    nn_search = re.compile(r'\wнн*\w\b')
+    
+    for sentence in data:
+        
+        places_with_tsya = tsya_search.search(sentence)
+        places_with_tisya = tsiya_search.search(sentence)
+        places_with_n = n_search.search(sentence)
+        places_with_nn = nn_search.search(sentence)
+        
+        if (places_with_tsya is not None) or (places_with_tisya is not None) or (places_with_n is not None) or (places_with_nn is not None):
+            data_with_tsya_or_nn.append(sentence)
+        
+    return data_with_tsya_or_nn
+    
 def main(path_file):
     
     data_processor = TestPreprocess()
@@ -28,19 +48,18 @@ def main(path_file):
         for i in range(num_of_sentences):
             text = input("Предложение: ")
             text_data.append(text)
-        
+       
     start_time = time.time()
-    input_ids, mask_ids, prediction_dataloader, nopad, label_ids = data_processor.process(text=text_data)
-
-    model = TsyaModel(weight_path = weight_path, train_from_chk = True)
-    
-    #if len(text_data) == 1:
-    #    predicts = model.predict_sentence(input_ids, mask_ids, nopad)
-    
-    predicts = model.predict_batch(prediction_dataloader, nopad)
-    output = ProcessOutput()
-    #incorrect, message, correct_text = output.process(predicts, input_ids, nopad, label_ids, text_data)
-    incorrect, message, correct_text = output.process(predicts, input_ids, nopad, text_data)
+    data_with_tsya_or_nn = check_contain_tsya_or_nn(text_data)
+    if len(data_with_tsya_or_nn) == 0:
+        message = ["Correct"]
+    else:
+        input_ids, mask_ids, prediction_dataloader, nopad, label_ids = data_processor.process(text=data_with_tsya_or_nn)
+        model = TsyaModel(weight_path = weight_path, train_from_chk = True)
+        predicts = model.predict_batch(prediction_dataloader, nopad)
+        output = ProcessOutput()
+        #incorrect, message, correct_text = output.process(predicts, input_ids, nopad, label_ids, text_data)
+        incorrect, message, correct_text = output.process(predicts, input_ids, nopad, data_with_tsya_or_nn)
     
     print('Elapsed time: ', time.time() - start_time)
 
